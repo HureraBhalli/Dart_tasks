bd933b18b6d1b3033cbaf199774b3534293b2230
Hurera Bhalli <hurerabhalli@gmail.com>
2024-03-12 02:49:07 +0500

    Updated

--- Task7.dart
+++ Task7.dart
@@ -7,9 +7,15 @@ void main() {
 
   if (ConsumedUnit < 100) {
     Finalprice = ConsumedUnit * 20;
+    print('Here is your Final Price: $Finalprice');
+    print('Here is the unit Consumed: $ConsumedUnit');
+    print('The unit cost was 20');
+  } else if (ConsumedUnit > 100 && ConsumedUnit < 200) {
+    Finalprice = ConsumedUnit * 30;
+    print('Here is your Final Price: $Finalprice');
+    print('Here is the unit Consumed: $ConsumedUnit');
+    print('The unit cost was 20');
+  } else {
+    print('Please write value in between 100 and 200');
   }
-
-  print('Here is your Final Price: $Finalprice');
-  print('Here is the unit Consumed: $ConsumedUnit');
-  print('The unit cost was 20');
 }
