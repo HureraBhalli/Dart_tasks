5f190565e977eece9c24cb32b3c36fc6bffc10d8
Hurera Bhalli <hurerabhalli@gmail.com>
2024-03-13 23:31:01 +0500

    Updated

---
+++ task9.dart
@@ -0,0 +1,14 @@
+void main() {
+  // 1st iteration i = 0
+  // 2nd iteration i = 1
+  // 3rd iteration i = 2
+  // 4th iteration i = 3
+  // 5th iteration i = 4
+  // 6th iteration i = 5
+
+  for (int i = 0; i < 5; i++) {
+    print('$i sqare ${i * i}');
+  }
+
+  print('--- The End ---');
+}
