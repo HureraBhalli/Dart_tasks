68144c1e6420eadba3a4133ac60859e1dc2109cf
Hurera Bhalli <hurerabhalli@gmail.com>
2024-03-16 22:43:21 +0500

    Updated

--- task12.dart
+++ task12.dart
@@ -1,8 +1,8 @@
 void main() {
   int x = 10;
-  int y = 40;
+  int y = 30;
   if (x + y == 40) {
-    print('Ja war pra');
+    print('Programme to war Gya naw');
   } else {
     print('Ok ho gya fer');
   }
