e7666d2003779d93d329b0b2018149f7e284ae18
Hurera Bhalli <hurerabhalli@gmail.com>
2024-03-19 20:25:54 +0500

    Updated

--- task26.dart
+++ task26.dart
@@ -2,6 +2,7 @@ void main() {
   Map<String, dynamic> map = {"name": " Bhalli ", 'age': 20};
 
   // map = {"name": " Bhalli ", 'age': 20};
+  // map = {"name": " Bhalli ", 'age': 20};
 
   print(map['age']);
 }
